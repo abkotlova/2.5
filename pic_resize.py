ee0edf0215ef80d3d79a8c8f19eb35bfed34c54d
Anna Kotlova <annakotlova@MacBook-Pro-Anna.local>
2018-01-24 00:34:17 +0300

    Initial commit

---
+++ pic_resize.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+
+def all_list():
+    source_dir = "Source"
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), source_dir))
+    files_list = os.listdir(path=".")   #os.listdir(path=".") - список файлов и директорий в папке.
+    return files_list
+
+
+def main(files_list):
+    print("---start program")
+    source_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Source")
+    result_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Result")
+    for i in files_list:
+        input_path = os.path.join(source_dir, i)
+        output_path = os.path.join(result_dir, i)
+        command = sips --resampleWidth 200 input_path cp output_path
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        subprocess.run(command)
+    print("----end program")
+
+
+main(all_list())
